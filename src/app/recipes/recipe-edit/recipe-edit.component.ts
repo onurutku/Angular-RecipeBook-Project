da12128fbdf9d80284829087a95de6c127de4f87
onurutku <topaloglu08@gmail.com>
2021-12-28 20:43:07 +0300

    recipe-edit page reactive formControl implement

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Data, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { RecipesService } from '../recipes.service';
@@ -10,6 +11,7 @@ import { Recipe } from './../recipe.model';
   styleUrls: ['./recipe-edit.component.css'],
 })
 export class RecipeEditComponent implements OnInit, OnDestroy {
+  editForm: FormGroup;
   message: string;
   id: number;
   recipe: Recipe;
@@ -27,12 +29,38 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
     this.paramsSubcription = this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
-      if (this.editMode === true) {
-        this.recipe = this.recipesService.getRecipe(this.id);
-      }
+      this.initForm();
     });
+  }
+  private initForm() {
+    if (this.editMode === true) {
+      this.recipe = this.recipesService.getRecipe(this.id);
+    }
+    const ingrediental = new FormArray([]);
+    if (this.recipe.ingredients) {
+      for (let ingredient of this.recipe.ingredients) {
+        ingrediental.push(
+          new FormGroup({
+            name: new FormControl(ingredient.name),
+            amount: new FormControl(ingredient.amount),
+          })
+        );
+      }
+    }
+    this.editForm = new FormGroup({
+      name: new FormControl(this.recipe.name),
+      title: new FormControl(this.recipe.desc),
+      imagePath: new FormControl(this.recipe.imagePath),
+      description: new FormControl(this.recipe.description),
+      ingredients: ingrediental,
+    });
+  }
+  getControls() {
+    return (<FormArray>this.editForm.get('ingredients')).controls;
   }
   ngOnDestroy(): void {
     this.paramsSubcription.unsubscribe();
   }
+  onSubmit() {}
+  addIngredient() {}
 }
